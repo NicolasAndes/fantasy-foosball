93a3d53fb092be1eb9ea04f916969a628c732413
nicolas <nicolas_andes26@yahoo.com>
2019-11-08 22:28:49 +0800

    Finish implementation of CRUD for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
+	before_action :find_user, only: [:show, :update, :edit, :destroy]
 
 	def index
+		@users = User.all.order("created_at DESC")
 	end
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -16,9 +26,27 @@ class UsersController < ApplicationController
 	end
 
 	def update
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@user.destroy
+
+		redirect_to users_path
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:firstname, :lastname)
+	end
+
+	def find_user
+		@user = User.find(params[:id])
 	end
 
 end
