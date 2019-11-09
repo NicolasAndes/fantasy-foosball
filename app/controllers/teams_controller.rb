a932edac13e1438a746c99b8e871e074a2a1ae78
nicolas <nicolas_andes26@yahoo.com>
2019-11-09 21:00:16 +0800

    Implement CRUD for Team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,51 @@
 class TeamsController < ApplicationController
+	before_action :find_team, only: [ :show, :update, :edit, :destroy ]
+
+	def index		
+		@teams = Team.all.order("created_at DESC")
+	end
+
+	def new
+		@team = Team.new
+	end
+
+	def create
+		@team = Team.new(team_params)
+
+		if @team.save
+			redirect_to @team
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @team.update(team_params)
+			redirect_to @team
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@team.destroy
+
+		redirect_to teams_path
+	end
+
+	private
+
+	def team_params
+		params.require(:team).permit(:name, :player) 
+	end
+
+	def find_team
+		@team = Team.find(params[:id])
+	end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+	validates :name, :player, presence: true
+	validates :player, numericality: { only_integer: true }
 end
